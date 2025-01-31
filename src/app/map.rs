55d27b7763693d4ec824dc24907c05863fd0bfd3
DIvAndrey <ivchenkoandrey2@gmail.com>
2025-01-31 21:06:58 +0500

    Fixed a bug in the last bugfix

--- src/app/map.rs
+++ src/app/map.rs
@@ -196,7 +196,7 @@ impl GameMap {
             from: (y1, x1),
             to: (y2, x2),
         } = m;
-        if y2 >= self.n || x2 >= self.m || x1 >= self.n || y1 >= self.m || x1.abs_diff(x2) + y1.abs_diff(y2) != 1 {
+        if y2 >= self.n || x2 >= self.m || y1 >= self.n || x1 >= self.m || x1.abs_diff(x2) + y1.abs_diff(y2) != 1 {
             return false;
         }
         let from = self.grid[y1][x1];
@@ -209,7 +209,7 @@ impl GameMap {
             from: (y1, x1),
             to: (y2, x2),
         } = m;
-        if y2 >= self.n || x2 >= self.m || x1 >= self.n || y1 >= self.m || x1.abs_diff(x2) + y1.abs_diff(y2) != 1 {
+        if y2 >= self.n || x2 >= self.m || y1 >= self.n || x1 >= self.m || x1.abs_diff(x2) + y1.abs_diff(y2) != 1 {
             return false;
         }
         let from = self.grid[y1][x1];
